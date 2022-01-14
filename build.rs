e9b1b03dc9e8205945e7e974a28184eae4f3911a
Jonathan Krebs <jonathan.krebs@bruckbu.de>
2022-01-14 13:48:09 +0100

    clean buildscript

--- build.rs
+++ build.rs
@@ -1,13 +1,11 @@
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+
     println!("cargo:rerun-if-changed=flagser.hpp");
     println!("cargo:rerun-if-changed=libflagser.cpp");
 
-     cc::Build::new()
+    cc::Build::new()
         .file("libflagser.cpp")
+        .cpp(true)
         .compile("flagser");
-    println!("cargo:rustc-link-lib=flagser");
-    println!("cargo:rustc-link-lib=stdc++");
 }
-
