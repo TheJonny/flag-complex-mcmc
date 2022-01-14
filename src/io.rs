a9dd67f91950c9c98ad48739d174c63b18885021
Florian Unger <florian.unger@fau.de>
2022-01-14 01:26:38 +0100

    added .flag file input parsing

---
+++ src/io.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+use crate::DirectedGraph;
+
+pub fn read_flag_file(fname:&str) -> DirectedGraph {
+    let mut file = File::open(fname).unwrap();
+    let mut fcontents = String::new();
+    file.read_to_string(&mut fcontents).unwrap();
+
+    let mut lines = fcontents.lines();
+    lines.next(); // skip dim 0
+    let nnodes = lines.next().unwrap().split(' ').filter(|s| *s != "").count(); // todo: mehr beleidigungen // todo: better parsing
+    let mut graph = DirectedGraph::new_disconnected(nnodes);
+    lines.next(); // skip dim 1
+    for line in lines {
+        let mut ijw = line.split(' ').filter(|s| *s != "");
+        if let (Some(i), Some(j)) = (ijw.next(), ijw.next()) {
+            graph.add_edge(i.parse().unwrap(), j.parse().unwrap());
+        }
+    }
+    return graph;
+}
