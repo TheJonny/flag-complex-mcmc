271641877bb6832e919ce4b6aecf72672ba759cd
Florian Unger <florian.unger@fau.de>
2022-01-17 12:58:41 +0100

    added BBP connectome as example

--- mcmc_sampling_examples/example_flag_generator.py
+++ mcmc_sampling_examples/example_flag_generator.py
@@ -1,5 +1,5 @@
-from pyflagser import save_unweighted_flag
-from data_importer import simplex, clique, c_elegans, densifier
+from data_importer import simplex, clique, bbp, c_elegans, densifier
+from scipy.sparse import coo_matrix
 
 import numpy as np
 
@@ -10,6 +10,18 @@ def join_graphs(a,b):
         [np.zeros(shape=(b.shape[0], a.shape[0])), b]
         ])
 
+def save_unweighted_flag(fname, graph):
+    assert(graph.shape[0] == graph.shape[1])
+    N = graph.shape[0]
+    content  = "dim 0:\n"
+    content += ("1 "*N)[:-1] + "\n"
+    content += "dim 1:\n"
+    coo_graph = coo_matrix(graph)
+    for i,j in zip(coo_graph.row, coo_graph.col):
+        content += f"{i} {j} 1\n"
+
+    with open(fname, 'w') as f:
+        f.write(content)
 
 if __name__ == '__main__':
     ex_00 = simplex(3)
@@ -39,13 +51,15 @@ if __name__ == '__main__':
 
     ex_08 = c_elegans()
 
+    ex_09 = bbp(0)
+
     examples = [ex_00, ex_01, ex_02, ex_03,
             ex_04, ex_05, ex_06,
             ex_07,
-            ex_08
+            ex_08, ex_09
             ]
 
     for i, ex in enumerate(examples):
-        print(i, ex)
+        #print(i, ex)
         save_unweighted_flag(f'/tmp/{i:02}.flag', ex)
 
