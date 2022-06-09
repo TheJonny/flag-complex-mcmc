5a341dd9d80dc676ed4704619236ec102d844282
Florian Unger <florian.unger@fau.de>
2022-06-09 13:42:38 +0200

    smallest(?) 7 node counterexample to exact seo-search

--- seo_search/sbatch_seo_counterexample_search.sh
+++ seo_search/sbatch_seo_counterexample_search.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH -J seo_counterexample_exact_search_small
-#SBATCH -a 000-049
+#SBATCH -J seo_counterexample_search_exact_small
+#SBATCH -a 000-035
 #SBATCH --cpus-per-task 1
 #SBATCH --output=slurmreports_exact_small/slurm-%A_%2a.out
 #SBATCH -p IGIcuda4
@@ -11,6 +11,6 @@ SATID=`printf %03d $SLURM_ARRAY_TASK_ID`
 #OUTDIR=/dev/null
 
 date
-../target/release/seo_search_counterexample -s $SATID -n 10 -p 0.8 -l 1000000 -x
+../target/release/seo_search_counterexample -s $SATID -n 7 -p 0.9 -l 50000 -x
 
 date
